5a2117f3435201b5c3b50084ac40a3f821a3141e
KavyaChandana20 <kavyanaiduu20@gmail.com>
2024-07-12 21:08:28 +0530

    commit

--- src/test/java/REST_API/tests/DataDrivenTesting/vwoLoginPOJO.java
+++ src/test/java/REST_API/tests/DataDrivenTesting/vwoLoginPOJO.java
@@ -8,7 +8,7 @@ public class vwoLoginPOJO
     /* Go to https://app.vwo.com/#/login , enter invalid password, click on sign in.
     Go to Network tab by inspecting the page, find Login ,click on it & copy the cURL (bah)
     Go to postman, create a new collection, then click on import & paste the cURL.
-    Go to Body in postman, click on beautify. Remove the username & password just keep it --> ""
+    Go to Body in postman, click on beautify. Remove the username & password just keep it --> "" [Negative testing]
     Now copy the body in postman & go to https://www.jsonschema2pojo.org/ give the class name.
     paste the body code, source type --> Json & Annotation--> gson , click on preview a code will be generated.
     Copy the code & create a java class in IntelliJ & paste it.  */
